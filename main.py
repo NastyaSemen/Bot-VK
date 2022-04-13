5e34748b29615d85a8a16eaf38152754f141e9f7
Nastya <semenikhina.a.d@gmail.com>
2022-04-13 23:30:03 +0300

    Каркас

--- main.py
+++ main.py
@@ -1,16 +1,76 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def write_msg(user_id, message):
+    vk.method('messages.send', {'user_id': user_id, 'message': message})
+
+token = "53904845f35b3574b6cfb52671845ce3966270399118973d87aa44340f45cef049c203a728749079f921f"
+
+vk = vk_api.VkApi(token=token)
+
+longpoll = VkLongPoll(vk)
+
+for event in longpoll.listen():
+
+    if event.type == VkEventType.MESSAGE_NEW:
+        if event.to_me:
+            request = event.text
+            if request == "привет":
+                write_msg(event.user_id, "Хай")
+            elif request == "пока":
+                write_msg(event.user_id, "Пока((")
+            else:
+                write_msg(event.user_id, "Не поняла вашего ответа...")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class VkBot:
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def __init__(self, user_id):
+        print("Создан объект бота!")
+        self._USER_ID = user_id
+        self._USERNAME = self._get_user_name_from_vk_id(user_id)
+
+        self._COMMANDS = ["ПРИВЕТ", "ЧТО ПОСМОТРЕТЬ", "ФИЛЬМ", "ЖАНР"]
+
+    def _get_user_name_from_vk_id(self, user_id):
+        request = requests.get("https://vk.com/id" + str(user_id))
+        bs = bs4.BeautifulSoup(request.text, "html.parser")
+
+        user_name = self._clean_all_tag_from_str(bs.findAll("title")[0])
+
+        return user_name.split()[0]
+
+    def new_message(self, message):
+
+        # Привет
+        if message.upper() == self._COMMANDS[0]:
+            return f"Привет-привет, {self._USERNAME}!"
+
+        # Погода
+        elif message.upper() == self._COMMANDS[1]:
+            return self._get_weather()
+
+        # Время
+        elif message.upper() == self._COMMANDS[2]:
+            return self._get_time()
+
+        # Пока
+        elif message.upper() == self._COMMANDS[3]:
+            return f"Пока-пока, {self._USERNAME}!"
+
+        else:
+            return "Не понимаю о чем вы..."
+
+
+print("Server started")
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW:
+        if event.to_me:
+            print('New message:')
+            print(f'For me by: {event.user_id}', end='')
+
+            bot = VkBot(event.user_id)
+            write_msg(event.user_id, bot.new_message(event.text))
+
+            print('Text: ', event.text)
