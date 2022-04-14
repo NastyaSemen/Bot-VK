7367ea47e36883ac8f5bd2ba8bf35f6b4fbb765c
Nastya <semenikhina.a.d@gmail.com>
2022-04-14 22:36:58 +0300

    1

--- main.py
+++ main.py
@@ -1,72 +1,37 @@
 import vk_api
+from vk_api .keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.longpoll import VkLongPoll, VkEventType
+from config import TOKEN
 
 
-def write_msg(user_id, message):
-    vk.method('messages.send', {'user_id': user_id, 'message': message})
+def send_message(user_id, message, keyboard=None):
+    post = {
+        'user_id': user_id,
+        'message': message,
+        'random_id': 0
+    }
 
-token = "53904845f35b3574b6cfb52671845ce3966270399118973d87aa44340f45cef049c203a728749079f921f"
+    if None != keyboard:
+        post['keyboard'] = keyboard.get_keyboard()
+    else:
+        post = post
 
-vk = vk_api.VkApi(token=token)
-
-longpoll = VkLongPoll(vk)
-
-for event in longpoll.listen():
-
-    if event.type == VkEventType.MESSAGE_NEW:
-        if event.to_me:
-            request = event.text
-            if request == "привет":
-                write_msg(event.user_id, "Хай")
-            elif request == "пока":
-                write_msg(event.user_id, "Пока((")
-            else:
-                write_msg(event.user_id, "Не поняла вашего ответа...")
+    session.method('messages.send', post)
 
 
-class VkBot:
+session = vk_api.VkApi(token=TOKEN)
 
-    def __init__(self, user_id):
-        print("Создан объект бота!")
-        self._USER_ID = user_id
-        self._USERNAME = self._get_user_name_from_vk_id(user_id)
+for event in VkLongPoll(session).listen():
+    if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+        user_id = event.user_id
+        text = event.text.lower()
 
-        self._COMMANDS = ["ПРИВЕТ", "ЧТО ПОСМОТРЕТЬ", "ФИЛЬМ", "ЖАНР"]
+        if text == 'привет':
+            send_message(user_id, 'Something text...')
 
-    def _get_user_name_from_vk_id(self, user_id):
-        request = requests.get("https://vk.com/id" + str(user_id))
-        bs = bs4.BeautifulSoup(request.text, "html.parser")
+        if text == 'start':
+            keyboard = VkKeyboard()
 
-        user_name = self._clean_all_tag_from_str(bs.findAll("title")[0])
+            keyboard.add_button('button', VkKeyboardColor.PRIMARY)
 
-        return user_name.split()[0]
-
-    def new_message(self, message):
-
-        if message.upper() == self._COMMANDS[0]:
-            return f"Привет-привет, {self._USERNAME}!"
-
-        elif message.upper() == self._COMMANDS[1]:
-            return self._get_weather()
-
-        elif message.upper() == self._COMMANDS[2]:
-            return self._get_time()
-
-        elif message.upper() == self._COMMANDS[3]:
-            return f"Пока-пока, {self._USERNAME}!"
-
-        else:
-            return "Не понимаю о чем вы..."
-
-
-print("Server started")
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
-        if event.to_me:
-            print('New message:')
-            print(f'For me by: {event.user_id}', end='')
-
-            bot = VkBot(event.user_id)
-            write_msg(event.user_id, bot.new_message(event.text))
-
-            print('Text: ', event.text)
+            send_message(user_id, 'The first button!')
