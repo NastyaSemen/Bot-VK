880acaae7dd751f44b2b7d668ddcf969ceeac5a2
Nastya <semenikhina.a.d@gmail.com>
2022-04-17 00:11:34 +0300

    1

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def send_message(user_id, message, keyboard=None):
         'random_id': 0
     }
 
-    if None != keyboard:
+    if keyboard != None:
         post['keyboard'] = keyboard.get_keyboard()
     else:
         post = post
@@ -22,16 +22,37 @@ def send_message(user_id, message, keyboard=None):
 session = vk_api.VkApi(token=TOKEN)
 
 for event in VkLongPoll(session).listen():
+
     if event.type == VkEventType.MESSAGE_NEW and event.to_me:
         user_id = event.user_id
         text = event.text.lower()
 
         if text == 'привет':
-            send_message(user_id, 'Something text...')
+            send_message(user_id, 'Чем займемся сегодня?')
 
         if text == 'start':
-            keyboard = VkKeyboard()
+            send_message(user_id, 'Привет, начинаем?')
+            keyboard = VkKeyboard(one_time=True)
 
-            keyboard.add_button('button', VkKeyboardColor.PRIMARY)
+            keyboard.add_button('Любой фильм', VkKeyboardColor.PRIMARY)
+            keyboard.add_button('Выбрать', VkKeyboardColor.SECONDARY)
+            keyboard.add_button('Топ 10', VkKeyboardColor.NEGATIVE)
 
-            send_message(user_id, 'The first button!')
+            send_message(user_id, 'Выберите интересующий вариант:', keyboard)
+
+            if text == 'Любой фильм':
+                pass
+
+            if text == 'Выбрать':
+                keyboard = VkKeyboard(one_time=True)
+
+                send_message(user_id, 'Какой жанр предпочитаете?')
+                keyboard.add_button('Ужасы', VkKeyboardColor.PRIMARY)
+                keyboard.add_button('Комедия', VkKeyboardColor.PRIMARY)
+                keyboard.add_button('Боевик', VkKeyboardColor.PRIMARY)
+
+                send_message(user_id, 'Выберите интересующий вариант:', keyboard)
+
+
+            if text == 'Топ 10':
+                pass
